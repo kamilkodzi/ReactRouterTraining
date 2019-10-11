c9d6d93574faf98e56362a852c3feb52c3e7289b
Adamski <Kamil.Adamski@unilever.com>
2019-10-11 15:56:40 +0200

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Card from './Card';
+
+const Home = (props) => {
+  return (
+    <div className="Grid animated bounceInUp">
+        {
+          props.cards.map((card) => (
+            <Card 
+              duration={150} 
+              key={card.id} 
+              card={card} 
+            />
+          ))
+        }
+      </div>
+  );
+}
+ 
+export default Home;
